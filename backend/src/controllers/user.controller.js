322cedcaf0b7b2cc68aa75f3e9f4a479b2a7ebc4
Tejas Birje <tejasbirje30@gmail.com>
2025-05-01 20:00:04 +0530

    friend request route and controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/friendrequest.model.js";
 import User from "../models/user.model.js";
 
 export async function getRecommendedUsers(req, res) {
@@ -7,9 +8,9 @@ export async function getRecommendedUsers(req, res) {
 
         const recommendedUsers = await User.find({
             $and: [
-                {_id: {$ne: currentUserId}}, // exclude yourself
-                {_id: {$nin: currentUser.friends}}, // exclude your friends
-                {isOnboarded: true}  // only get onboarded users
+                { _id: { $ne: currentUserId } }, // exclude yourself
+                { _id: { $nin: currentUser.friends } }, // exclude your friends
+                { isOnboarded: true }  // only get onboarded users
             ]
         });
 
@@ -23,12 +24,72 @@ export async function getRecommendedUsers(req, res) {
 export async function getMyFriends(req, res) {
     try {
         const user = await User.findById(req.user.id)
-        .select("friends")
-        .populate("friends", "fullName profilePic nativeLanguage learningLanguage");
+            .select("friends")
+            .populate("friends", "fullName profilePic nativeLanguage learningLanguage");
 
         res.status(200).json(user.friends);
     } catch (error) {
         console.log("Error in getMyFriends controller: ", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+/* 
+
+    getMyFriends() explained: 
+
+    1)findById() gets the user document.
+    2) .select("friends") limits the fields returned to just the friends array.
+    3) .populate("friends", ...) replaces each friend's ID with their actual user object, but only with selected fields:
+        fullName
+        profilePic
+        nativeLanguage
+        learningLanguage
+This gives you a fully expanded list of friends with useful info, not just raw IDs.
+
+*/
+
+export async function sendFriendRequest(req, res) {
+    try {
+        const myId = req.user.id;
+        const { id: recipientId } = req.params;
+
+        // prevent sending request to yourself
+        if (!myId === recipientId) {
+            return res.status(400).json({ message: "You cannot send request to yourself" });
+        }
+
+        // check if recipient exists
+        const recipient = await User.findById(recipientId);
+        if (!recipient) {
+            return res.status(404).json({ message: "Recipient not found" });
+        }
+
+        // check if recipient is already your friend
+        if (recipient.friends.includes(myId)) {
+            return res.status(400).json({ message: "You are already friends with them" });
+        }
+
+        // check if a request is already sent
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },  // the request I sent to them
+                { sender: recipient, recipient: myId }  // the request they sent to me 
+            ]
+        })
+
+        if(existingRequest) {
+            return res.status(400).json({ message: "You have already sent this user a friend request" });
+        }
+
+        const friendRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId,
+        });
+
+        res.status(201).json(friendRequest);
+
+    } catch (error) {
+        console.log("Error in sendFriendRequest controller: ", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js";
+import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.use(protectRoute); // apply auth middleware to all user routes
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
 
 export default router;
