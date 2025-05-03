4c9e8a6e2384d03770fc8f5300c08f0b1f061499
Tejas Birje <tejasbirje30@gmail.com>
2025-05-04 00:07:27 +0530

    deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,14 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -23,6 +26,14 @@ app.use(cookieParser()); // so we have access to the cookies in the request
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 // Starts the Express server and listens for incoming requests on a specific port.
 app.listen(PORT, () => {
